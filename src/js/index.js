a2d1da7b44d4131aaf5a3d774d4d233f80f8bf8d
sarbash-danil <75271263+sarbash-danil@users.noreply.github.com>
2021-05-20 21:22:02 +0300

    workingWeatherLocation

--- src/js/index.js
+++ src/js/index.js
@@ -6,102 +6,12 @@
 import "@babel/polyfill";
 // import { get } from "jquery";
 import{BtnPopup,BtnDropMenu,BtnScroll} from './actions.js'
-const axios = require('axios').default;
+import{APIService, Application} from './weather.js'
 
-  class APIService {
-    constructor() {
-    }
-    getPosition(){
-      (this.getLocation=(position)=>{
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(this.getLocation);
-        } else {
-          showError("Your browser does not support Geolocation!");
-        }
-        var lat = position.coords.latitude; 
-        var lng = position.coords.longitude;
-        console.log(lat);
-        console.log(lng);
-      })()
-        
-    }
-   getCityName(position) {
-      // var lat = position.coords.latitude;  // Закоментил, что б не ругалось, вообще должно быть раскоменченное 
-      // var lng = position.coords.longitude; // Закоментил, что б не ругалось, вообще должно быть раскоменченное 
-        
-        axios({
-          method: 'post',
-          url: `https://www.mapquestapi.com/geocoding/v1/reverse?key=1rgOn51ZPNAMn2lAQvX8yQkB5rib5hKV`,
-          data:{
-              location: {
-                  latLng: {
-                      // lat: `${lat}`, // Закоментил, что б не ругалось, вообще должно быть раскоменченное 
-                      // lng: `${lng}`  // Закоментил, что б не ругалось, вообще должно быть раскоменченное 
-                  }
-              },
-              options: {
-                  thumbMaps: false
-              }
-          }
-      })
-      
-        .then((responce) => {
-            var city = responce.data.results[0].locations[0].adminArea5 //!
-            var cityStr = JSON.stringify(city)
-            this.userCity = cityStr.split('"').join('')
-            
-        })
-        .catch((error) =>{
-            // console.error(error);
-        })
-        
-      }
-      
-   }
-  
-  class Application extends APIService {
-    constructor() {
-      super(apiService.userCity)
-    }
-    async showLocalWeather(){
-      this.setWeather(apiService.userCity)
-    }
-    async showDefaultCity(){
-      this.setWeather('Dubai')
-    }
-    async setWeather(cityName){
-      let weatherResponce = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=add83517209f776fcced4e6690e72a82`)
-      let data = weatherResponce.json()
-      .then((data) => {
-          console.log(data);
-          document.querySelector('.city').innerHTML = data.name;
-          document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + " ℃";
-          document.querySelector('.status').innerHTML = data.weather[0].description;
-          document.querySelector('.wind').innerHTML = "скорость ветра " + data.wind.speed;
-          document.querySelector('.icon-weather').innerHTML = `<img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
-      })
-      .catch((error) =>{
-        console.error(error);
-    })
-      return data
-    }
-    renderData(){
-      document.querySelector('.btn__head').addEventListener('click',() => {
-        this.showDefaultCity()
-        
-      })  
-      document.querySelector(".weather__btn").addEventListener('click',() => {
-        this.showLocalWeather();
-      }) 
-    }
-  }
-  const apiService = new APIService;
-  const application = new Application();
+
+const application = new Application();
   application.renderData()
-  // apiService.getLocation()
-  apiService.getCityName()
-  apiService.getPosition()
-
+  
   BtnPopup()
   BtnDropMenu()
   BtnScroll()
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,74 +1,88 @@
-    // const axios = require('axios').default;
-    // let city;
-    // let cityStr;
-    // var deleteSymbols;
+const axios = require('axios').default;
+
+    export class APIService {
+        constructor() {
+        }
+        getPosition() {
+          return new Promise(function(resolve, reject){
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+        }
+          async getCoordinates() {
+            const position = await this.getPosition();
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
     
-//    export class APIservice{
-//     constructor() {
+            return { lat, lng };
+          }
+       getCityName(lat, lng) {
+            axios({
+              method: 'post',
+              url: `https://www.mapquestapi.com/geocoding/v1/reverse?key=1rgOn51ZPNAMn2lAQvX8yQkB5rib5hKV`,
+              data:{
+                  location: {
+                      latLng: {
+                          lat: lat, 
+                          lng: lng 
+                      }
+                  },
+                  options: {
+                      thumbMaps: false
+                  }
+              }
+          })
+            .then((response) => {
+              console.log(response.data);
+                var city = response.data.results[0].locations[0].adminArea5 //!
+                var cityStr = JSON.stringify(city)
+                this.userCity = cityStr.split('"').join('')
+                
+            })
+            .catch((error) =>{
+                console.error(error);
+            })
+          }
+       }
+      
+       export class Application extends APIService {
+        constructor() {
+          super(apiService.userCity)
+        }
+        async showLocalWeather(){
+          this.setWeather(apiService.userCity)
+        }
+        async showDefaultCity(){
+          this.setWeather('Dubai')
+        }
+        async setWeather(cityName){
+          let weatherResponce = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=add83517209f776fcced4e6690e72a82`)
+          let data = weatherResponce.json()
+          .then((data) => {
+              document.querySelector('.city').innerHTML = data.name;
+              document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + " ℃";
+              document.querySelector('.status').innerHTML = data.weather[0].description;
+              document.querySelector('.wind').innerHTML = "скорость ветра " + data.wind.speed;
+              document.querySelector('.icon-weather').innerHTML = `<img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+          })
+          .catch((error) =>{
+            console.error(error);
+        })
+          return data
+        }
+        renderData(){
+          document.querySelector('.btn__head').addEventListener('click',() => {
+            this.showDefaultCity()
+            
+          })  
+          document.querySelector(".weather__btn").addEventListener('click',() => {
+            this.showLocalWeather();
+            apiService.getCoordinates().then(({ lat, lng })=>{
+              apiService.getCityName(lat, lng)
+            });
+            apiService.getPosition()
+          }) 
+        }
+      }
 
-//       }
-//       showPosition(position) {
-//         const lat = 49.2305236; //position.coords.latitude;
-//         const lng = 28.4388634; //position.coords.longitude
-//         return new Promise((resolve, reject) => {
-//         axios({
-//             method: 'post',
-//             url: 'https://www.mapquestapi.com/geocoding/v1/reverse?key=1rgOn51ZPNAMn2lAQvX8yQkB5rib5hKV',
-//             data:{
-//                 location: {
-//                     latLng: {
-//                         lat: `${lat}`,
-//                         lng: `${lng}`
-//                     }
-//                 },
-//                 options: {
-//                     thumbMaps: false
-//                 }
-//             }
-//         })
-//         .then((resolve) => {
-//             city = resolve.data.results[0].locations[0].adminArea5
-//             cityStr = JSON.stringify(city)
-//             deleteSymbols = cityStr.split('"').join('')
-//         })
-//         .catch((error) =>{
-//             console.error(error);
-//         })
-//         })
-       
-//         }
-//        getLocation(resolve){
-        
-//           if (navigator.geolocation) {
-//               navigator.geolocation.getCurrentPosition(this.showPosition);
-//           } else {
-//               x.innerHTML = "Geolocation is not supported by this browser.";
-//           }
-//       }
-//       showLocalWeather(){
-//         this.showKiev(deleteSymbols)
-//       }
-//       showKievWeather(){
-//         this.showKiev('Kiev')
-//       }
-//       showKiev(cityName){
-          
-//           fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=add83517209f776fcced4e6690e72a82`)
-//           .then((resp) => { return resp.json() })
-//           .then((data) => {
-//               console.log(data);
-//               document.querySelector('.city').innerHTML = data.name;
-//               document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + " ℃";
-//               document.querySelector('.status').innerHTML = data.weather[0].description;
-//               document.querySelector('.wind').innerHTML = "скорость ветра " + data.wind.speed;
-//               document.querySelector('.icon-weather').innerHTML = `<img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
-//           })
-//           .catch(()=> {alert("check your location`s permitions") })
-//       }
-
-//     }
-
-
-
-    
-    
+      const apiService = new APIService();
+     
